bd5d43c6283c6dc80d2b57635c82ad07b1f6989b
FecklessOtter <ianmiclean4@gmail.com>
2025-02-13 13:21:54 -0500

    add particle effect

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-
+// Ensure this code runs after the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize particles.js with your configuration
+    particlesJS.load('particles-js', 'resources/particles-config.json', function() {
+      console.log('particles.js config loaded');
+    });
+  });
+  
   
